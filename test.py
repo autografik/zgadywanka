67fd63c07f3a963d155139de680982f67fe29e99
Autografik <autografik@icloud.com>
2023-06-19 18:47:41 +0200

    nothing important

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-'''
+
+nums = []
+guessed = [] 
+
 while True:
     try:
         x = int(input("Please enter a number: "))
         print(type(x))
     except ValueError:
          print("Oops!  That was no valid number.  Try again...")
-'''
-msg = "Hello World"
-print(msg)
